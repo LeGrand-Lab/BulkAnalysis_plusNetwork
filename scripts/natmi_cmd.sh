9a4b62389e3f73410d6a9d5daae7fa382a9ff915
johaGL <deisy.lascroux@etu.univ-lyon1.fr>
2021-04-22 11:34:29 +0200

    Natmi following Dr Hou advice

--- scripts/natmi_cmd.sh
+++ scripts/natmi_cmd.sh
@@ -1,12 +1,13 @@
 
 cd ~/NATMI
 
-for i in $(ls ~/BulkAnalysis_plusNetwork/data/meanTPM_*); do 
+for i in $(ls ~/BulkAnalysis_plusNetwork/inDataNatmi/TPM_*); do 
 	echo $i | awk -F '[._]' '{print $3}'
 	CW=$(echo $i | awk -F '[._]' '{print $3}')
 	python ExtractEdges.py \
 		--species mouse \
-		--emFile ~/BulkAnalysis_plusNetwork/data/meanTPM_${CW}.txt \
+		--emFile ~/BulkAnalysis_plusNetwork/inDataNatmi/TPM_${CW}.txt \
+		--annFile ~/BulkAnalysis_plusNetwork/inDataNatmi/annot_${CW}.txt\
 		--interDB lrc2p \
 		--idType ensembl \
 		--out ~/BulkAnalysis_plusNetwork/natmiOut/${CW}/ \
@@ -14,10 +15,4 @@ for i in $(ls ~/BulkAnalysis_plusNetwork/data/meanTPM_*); do
 	echo ""
 done
 
-#python ExtractEdges.py \
-#  --species mouse \
-#  --emFile ~/bulk_analysis/data/meanTPMYoungD7.txt \
-#  --interDB lrc2p \
-#  --idType ensembl \
-#  --out ~/bulk_analysis/natmiOut/Young_D7/ \
-#  --coreNum 4
+
---
+++ scripts/natmi_edges.sh
@@ -0,0 +1,23 @@
+
+cd ~/NATMI
+
+for i in $(ls ~/BulkAnalysis_plusNetwork/inDataNatmi/TPM_*); do 
+	echo $i | awk -F '[_.]' '{print $3}'
+	CW=$(echo $i | awk -F '[_.]' '{print $3}')
+	python VisInteractions.py \
+	  --sourceFolder ~/BulkAnalysis_plusNetwork/natmiOut/${CW}/ \
+	  --interDB lrc2p \
+	  --weightType mean \
+	  --detectionThreshold 0.6 \
+	  --plotFormat pdf \
+	  --drawNetwork y \
+	  --plotWidth 12 \
+	  --plotHeight 10 \
+	  --layout kk \
+	  --fontSize 8 \
+	  --edgeWidth 0 --maxClusterSize 0 --clusterDistance 1 
+	echo ""
+done
+
+
+
