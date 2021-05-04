6a31640244246d28e227e9ad57a742b3c6879ed7
JohaGL <deisy.lascroux@etu.univ-lyon1.fr>
2021-05-04 17:20:14 +0200

    organize network part

--- graphmodeling/myminigraph.ml
+++ graphmodeling/myminigraph.ml
@@ -15,36 +15,36 @@
   <key id="e_weight" for="edge" attr.name="weight" attr.type="double"/>
   <graph id="G" edgedefault="directed">
     <node id="n0">
-      <data key="v__nx_name">Cd22_M2</data>
-      <data key="v_nodetype">sender</data>
-      <data key="v_celltype">M2</data>
-      <data key="v_genesym">Cd22</data>
-      <data key="v_color">#E66101</data>
-      <data key="v_averagexp">8.91760664792547</data>
-    </node>
-    <node id="n1">
-      <data key="v__nx_name">Mrc1_M1</data>
-      <data key="v_nodetype">sender</data>
-      <data key="v_celltype">M1</data>
-      <data key="v_genesym">Mrc1</data>
-      <data key="v_color">#13306db2</data>
-      <data key="v_averagexp">170.634309362503</data>
-    </node>
-    <node id="n2">
-      <data key="v__nx_name">Cd69_M2</data>
-      <data key="v_nodetype">receiver</data>
-      <data key="v_celltype">M2</data>
-      <data key="v_genesym">Cd69</data>
-      <data key="v_color">#E66101</data>
-      <data key="v_averagexp">18.1581705777213</data>
-    </node>
-    <node id="n3">
       <data key="v__nx_name">Ptprc_M1</data>
       <data key="v_nodetype">receiver</data>
       <data key="v_celltype">M1</data>
       <data key="v_genesym">Ptprc</data>
       <data key="v_color">#13306db2</data>
-      <data key="v_averagexp">201.725524389746</data>
+      <data key="v_averagexp">236.563113</data>
+    </node>
+    <node id="n1">
+      <data key="v__nx_name">Cd22_M1</data>
+      <data key="v_nodetype">sender</data>
+      <data key="v_celltype">M1</data>
+      <data key="v_genesym">Cd22</data>
+      <data key="v_color">#13306db2</data>
+      <data key="v_averagexp">6.090704</data>
+    </node>
+    <node id="n2">
+      <data key="v__nx_name">Cd22_M2</data>
+      <data key="v_nodetype">sender</data>
+      <data key="v_celltype">M2</data>
+      <data key="v_genesym">Cd22</data>
+      <data key="v_color">#E66101</data>
+      <data key="v_averagexp">9.57952533333334</data>
+    </node>
+    <node id="n3">
+      <data key="v__nx_name">Cd69_M2</data>
+      <data key="v_nodetype">receiver</data>
+      <data key="v_celltype">M2</data>
+      <data key="v_genesym">Cd69</data>
+      <data key="v_color">#E66101</data>
+      <data key="v_averagexp">18.7550626666667</data>
     </node>
     <node id="n4">
       <data key="v__nx_name">Lgals1_FAPs</data>
@@ -52,31 +52,31 @@
       <data key="v_celltype">FAPs</data>
       <data key="v_genesym">Lgals1</data>
       <data key="v_color">#5F9EA0</data>
-      <data key="v_averagexp">240.747380062863</data>
+      <data key="v_averagexp">258.835891666667</data>
     </node>
     <node id="n5">
+      <data key="v__nx_name">Ptprc_M2</data>
+      <data key="v_nodetype">receiver</data>
+      <data key="v_celltype">M2</data>
+      <data key="v_genesym">Ptprc</data>
+      <data key="v_color">#E66101</data>
+      <data key="v_averagexp">335.365081666667</data>
+    </node>
+    <node id="n6">
       <data key="v__nx_name">Myl9_sCs</data>
       <data key="v_nodetype">sender</data>
       <data key="v_celltype">sCs</data>
       <data key="v_genesym">Myl9</data>
       <data key="v_color">#DFC27D</data>
-      <data key="v_averagexp">57.3772571889762</data>
+      <data key="v_averagexp">58.7497215</data>
     </node>
-    <node id="n6">
+    <node id="n7">
       <data key="v__nx_name">Cd69_M1</data>
       <data key="v_nodetype">receiver</data>
       <data key="v_celltype">M1</data>
       <data key="v_genesym">Cd69</data>
       <data key="v_color">#13306db2</data>
-      <data key="v_averagexp">10.6364862150641</data>
-    </node>
-    <node id="n7">
-      <data key="v__nx_name">Cd22_M1</data>
-      <data key="v_nodetype">sender</data>
-      <data key="v_celltype">M1</data>
-      <data key="v_genesym">Cd22</data>
-      <data key="v_color">#13306db2</data>
-      <data key="v_averagexp">3.86793984054889</data>
+      <data key="v_averagexp">10.8702826666667</data>
     </node>
     <node id="n8">
       <data key="v__nx_name">Mrc1_M2</data>
@@ -84,125 +84,125 @@
       <data key="v_celltype">M2</data>
       <data key="v_genesym">Mrc1</data>
       <data key="v_color">#E66101</data>
-      <data key="v_averagexp">155.715100751252</data>
+      <data key="v_averagexp">159.482963666667</data>
     </node>
     <node id="n9">
-      <data key="v__nx_name">Ptprc_M2</data>
-      <data key="v_nodetype">receiver</data>
-      <data key="v_celltype">M2</data>
-      <data key="v_genesym">Ptprc</data>
-      <data key="v_color">#E66101</data>
-      <data key="v_averagexp">334.627309836981</data>
+      <data key="v__nx_name">Mrc1_M1</data>
+      <data key="v_nodetype">sender</data>
+      <data key="v_celltype">M1</data>
+      <data key="v_genesym">Mrc1</data>
+      <data key="v_color">#13306db2</data>
+      <data key="v_averagexp">174.566986333333</data>
     </node>
-    <edge source="n0" target="n3">
-      <data key="e_origtype">Cd22_M2</data>
-      <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.261815178535628</data>
+    <edge source="n0" target="n1">
+      <data key="e_origtype">Ptprc_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.159393885511652</data>
+    </edge>
+    <edge source="n0" target="n2">
+      <data key="e_origtype">Ptprc_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.250696432503911</data>
     </edge>
     <edge source="n0" target="n9">
+      <data key="e_origtype">Ptprc_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.214077395984408</data>
+    </edge>
+    <edge source="n0" target="n8">
+      <data key="e_origtype">Ptprc_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.19557934912414</data>
+    </edge>
+    <edge source="n1" target="n0">
+      <data key="e_origtype">Cd22_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.159393885511652</data>
+    </edge>
+    <edge source="n1" target="n5">
+      <data key="e_origtype">Cd22_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.225965674672967</data>
+    </edge>
+    <edge source="n2" target="n0">
       <data key="e_origtype">Cd22_M2</data>
       <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.434305520498225</data>
+      <data key="e_weight">0.250696432503911</data>
     </edge>
-    <edge source="n1" target="n3">
-      <data key="e_origtype">Mrc1_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.195887116908892</data>
+    <edge source="n2" target="n5">
+      <data key="e_origtype">Cd22_M2</data>
+      <data key="e_ecolor">#E66101</data>
+      <data key="e_weight">0.355401264778825</data>
     </edge>
-    <edge source="n1" target="n9">
-      <data key="e_origtype">Mrc1_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.324942414507248</data>
+    <edge source="n4" target="n0">
+      <data key="e_origtype">Lgals1_FAPs</data>
+      <data key="e_ecolor">#5F9EA0</data>
+      <data key="e_weight">0.129544766393973</data>
     </edge>
-    <edge source="n3" target="n7">
-      <data key="e_origtype">Ptprc_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.113560218554152</data>
+    <edge source="n4" target="n5">
+      <data key="e_origtype">Lgals1_FAPs</data>
+      <data key="e_ecolor">#5F9EA0</data>
+      <data key="e_weight">0.183649896259202</data>
     </edge>
-    <edge source="n3" target="n0">
-      <data key="e_origtype">Ptprc_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.261815178535628</data>
-    </edge>
-    <edge source="n3" target="n1">
-      <data key="e_origtype">Ptprc_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.195887116908892</data>
-    </edge>
-    <edge source="n3" target="n8">
-      <data key="e_origtype">Ptprc_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.178759958998278</data>
+    <edge source="n4" target="n7">
+      <data key="e_origtype">Lgals1_FAPs</data>
+      <data key="e_ecolor">#5F9EA0</data>
+      <data key="e_weight">0.114357518062118</data>
     </edge>
     <edge source="n4" target="n3">
       <data key="e_origtype">Lgals1_FAPs</data>
       <data key="e_ecolor">#5F9EA0</data>
-      <data key="e_weight">0.118906628226941</data>
+      <data key="e_weight">0.197306959113067</data>
     </edge>
-    <edge source="n4" target="n9">
-      <data key="e_origtype">Lgals1_FAPs</data>
-      <data key="e_ecolor">#5F9EA0</data>
-      <data key="e_weight">0.197245268023157</data>
-    </edge>
-    <edge source="n4" target="n6">
-      <data key="e_origtype">Lgals1_FAPs</data>
-      <data key="e_ecolor">#5F9EA0</data>
-      <data key="e_weight">0.116417314167491</data>
-    </edge>
-    <edge source="n4" target="n2">
-      <data key="e_origtype">Lgals1_FAPs</data>
-      <data key="e_ecolor">#5F9EA0</data>
-      <data key="e_weight">0.198742837259507</data>
-    </edge>
-    <edge source="n5" target="n6">
-      <data key="e_origtype">Myl9_sCs</data>
-      <data key="e_ecolor">#DFC27D</data>
-      <data key="e_weight">0.296885667513956</data>
+    <edge source="n5" target="n1">
+      <data key="e_origtype">Ptprc_M2</data>
+      <data key="e_ecolor">#E66101</data>
+      <data key="e_weight">0.225965674672967</data>
     </edge>
     <edge source="n5" target="n2">
+      <data key="e_origtype">Ptprc_M2</data>
+      <data key="e_ecolor">#E66101</data>
+      <data key="e_weight">0.355401264778825</data>
+    </edge>
+    <edge source="n5" target="n9">
+      <data key="e_origtype">Ptprc_M2</data>
+      <data key="e_ecolor">#E66101</data>
+      <data key="e_weight">0.303488073338375</data>
+    </edge>
+    <edge source="n5" target="n8">
+      <data key="e_origtype">Ptprc_M2</data>
+      <data key="e_ecolor">#E66101</data>
+      <data key="e_weight">0.277264208944235</data>
+    </edge>
+    <edge source="n6" target="n7">
       <data key="e_origtype">Myl9_sCs</data>
       <data key="e_ecolor">#DFC27D</data>
-      <data key="e_weight">0.506830966899964</data>
+      <data key="e_weight">0.286555287963766</data>
     </edge>
-    <edge source="n7" target="n3">
-      <data key="e_origtype">Cd22_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.113560218554152</data>
+    <edge source="n6" target="n3">
+      <data key="e_origtype">Myl9_sCs</data>
+      <data key="e_ecolor">#DFC27D</data>
+      <data key="e_weight">0.494408705645293</data>
     </edge>
-    <edge source="n7" target="n9">
-      <data key="e_origtype">Cd22_M1</data>
-      <data key="e_ecolor">#13306db2</data>
-      <data key="e_weight">0.188376510876514</data>
-    </edge>
-    <edge source="n8" target="n3">
+    <edge source="n8" target="n0">
       <data key="e_origtype">Mrc1_M2</data>
       <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.178759958998278</data>
+      <data key="e_weight">0.19557934912414</data>
     </edge>
-    <edge source="n8" target="n9">
+    <edge source="n8" target="n5">
       <data key="e_origtype">Mrc1_M2</data>
       <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.296531459601467</data>
-    </edge>
-    <edge source="n9" target="n7">
-      <data key="e_origtype">Ptprc_M2</data>
-      <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.188376510876514</data>
+      <data key="e_weight">0.277264208944235</data>
     </edge>
     <edge source="n9" target="n0">
-      <data key="e_origtype">Ptprc_M2</data>
-      <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.434305520498225</data>
+      <data key="e_origtype">Mrc1_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.214077395984408</data>
     </edge>
-    <edge source="n9" target="n1">
-      <data key="e_origtype">Ptprc_M2</data>
-      <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.324942414507248</data>
-    </edge>
-    <edge source="n9" target="n8">
-      <data key="e_origtype">Ptprc_M2</data>
-      <data key="e_ecolor">#E66101</data>
-      <data key="e_weight">0.296531459601467</data>
+    <edge source="n9" target="n5">
+      <data key="e_origtype">Mrc1_M1</data>
+      <data key="e_ecolor">#13306db2</data>
+      <data key="e_weight">0.303488073338375</data>
     </edge>
   </graph>
 </graphml>
